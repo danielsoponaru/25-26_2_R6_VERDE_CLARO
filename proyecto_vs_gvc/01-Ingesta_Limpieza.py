49aa6d612f7a129be1545eb411be9682d37955bf
Aiala Etxebarrieta <aiala.etxebarrieta@alumni.mondragon.edu>
2025-11-26 17:26:17 +0100

    he empezado a hacer la limpueza de datos y conversion de variables

--- proyecto_vs_gvc/01-Ingesta_Limpieza.py
+++ proyecto_vs_gvc/01-Ingesta_Limpieza.py
@@ -1,25 +1,51 @@
-# INGESTA
-# In: Datos originales
-# Out: Datos limpios
+import pandas as pd
+import numpy as np
 
-import packages.Preprocesamiento as ppr
-import os
+df = pd.read_csv("DATOS/cancellation_data_for_mondragon_unibertsitatea_2024.csv")
+print(df)
 
-# Load
-path_datos = os.path.join('Datos','Originales')
-filename = os.path.join(path_datos,'titanic.csv')
-df = ppr.read_data(filename)
-#print(df.head())
+#limpiar columnas
+df.columns = (
+    df.columns.str.strip()
+    .str.lower()
+    .str.replace(" ", "_")
+    .str.replace(r"[^\w_]", "", regex=True)
+)
 
-# Clean
-df = ppr.remove_rows_with_nas(df, 'Age')
+#eliminar duplicados
+df = df.drop_duplicates()
 
-# Folder to save clean data
-path_clean_data = os.path.join('Datos','Limpios')
-if not os.path.exists(path_clean_data):
-  os.makedirs(path_clean_data)
+#convertir en fecha
+columnas_fecha = [
+    "booked_at",
+    "checkin_time",
+    "checkout_time",
+    "cancelled_at"
+]
+
+for col in columnas_fecha:
+    if col in df.columns:
+        df[col] = pd.to_datetime(df[col], errors="coerce")
+#convertir checkin_month en números del 1 al 12 y checkin_day del 1 al 7
+import pandas as pd
+
+#meses
+month_map = {
+    "January": 1, "February": 2, "March": 3, "April": 4,
+    "May": 5, "June": 6, "July": 7, "August": 8,
+    "September": 9, "October": 10, "November": 11, "December": 12
+}
+
+#días
+day_map = {
+    "Monday": 1, "Tuesday": 2, "Wednesday": 3,
+    "Thursday": 4, "Friday": 5, "Saturday": 6, "Sunday": 7
+}
+
+df["checkin_month"] = df["checkin_month"].map(month_map)
+df["checkin_day"]   = df["checkin_day"].map(day_map)
+
+#no imputamos porque los missings tienen sentido. no va a haber fecha de cancelación si no ha cancelado y tampoco va a haber chekout_time si lo ha cancelado
 
 
-file_clean = os.path.join(path_clean_data,'titanic_limpios_1.csv')
-ppr.save_clean_data(df, file_clean)
 
